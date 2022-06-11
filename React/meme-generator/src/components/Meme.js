3fcb3e4a3ff353f12ddfa0ae9f25f8ba0a1c5988
AndresEfQ <andresf2641@gmail.com>
2022-06-10 22:14:30 -0500

    Complete Header and form style

--- React/meme-generator/src/components/Meme.js
+++ React/meme-generator/src/components/Meme.js
@@ -3,13 +3,15 @@ import "./meme.css";
 
 function Meme() {
   return (
-    <form>
-      <div className="inputs">
-        <input type="text" name="upper" id="upper" value="Shut up" />
-        <input type="text" name="lower" id="lower" value="and take my money" />
-      </div>
-      <input type="button" value="Get a new meme image 🖼" />
-    </form>
+    <main>
+      <form>
+        <div className="inputs">
+          <input type="text" name="upper" id="upper" placeholder="Top text" />
+          <input type="text" name="lower" id="lower" placeholder="Bottom text" />
+        </div>
+        <input type="button" value="Get a new meme image 🖼" />
+      </form>
+    </main>
   )
 }
 
