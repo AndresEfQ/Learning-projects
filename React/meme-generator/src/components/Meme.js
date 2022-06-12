2b224f20e17400c1b5c91dc3d35c14859039fe99
AndresEfQ <andresf2641@gmail.com>
2022-06-12 12:04:32 -0500

    Complete image and text logic

--- React/meme-generator/src/components/Meme.js
+++ React/meme-generator/src/components/Meme.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./meme.css";
 import Data from "../memesData";
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  const [memeImage, setMemeImage] = useState("");
+  const [upperText, setUpperText] = useState("");
+  const [lowerText, setLowerText] = useState("");
+
   let image;
   if (memeImage) {
-    image = <img src={memeImage} alt="Meme" />
+    image = <img src="https://i.imgflip.com/3si4.jpg" alt="Meme" />
   } else {
     image = null;
   }
@@ -16,20 +19,30 @@ function Meme() {
     const randomNumb = Math.floor(Math.random() * memesArray.length);
     const {url} = memesArray[randomNumb];
     setMemeImage(url)
-    console.log(memeImage);
+  }
+
+  function insertUpperText(e) {
+    setUpperText(() => e.target.value.toUpperCase());
+    console.log(e.target.value)
+  }
+
+  function insertLoweText(e) {
+    setLowerText(() => e.target.value.toUpperCase());
   }
 
   return (
     <main>
       <form>
         <div className="inputs">
-          <input type="text" name="upper" id="upper" placeholder="Top text" />
-          <input type="text" name="lower" id="lower" placeholder="Bottom text" />
+          <input type="area" name="upper" id="upper" placeholder="Top text" onChange={insertUpperText}/>
+          <input type="text" name="lower" id="lower" placeholder="Bottom text" onChange={insertLoweText} />
         </div>
         <input type="button" value="Get a new meme image 🖼" onClick={getMemeImage} />
       </form>
       <div className="image">
         {image}
+        <div className="upper-text">{upperText}</div>
+        <div className="lower-text">{lowerText}</div>
       </div>
     </main>
   )
