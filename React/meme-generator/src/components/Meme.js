f753a8e7685c5451145f25c52b91f266b27bc04f
AndresEfQ <andresf2641@gmail.com>
2022-06-22 08:26:10 -0500

    Change get memes to async

--- React/meme-generator/src/components/Meme.js
+++ React/meme-generator/src/components/Meme.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./meme.css";
 
 function Meme() {
@@ -10,10 +10,13 @@ function Meme() {
   });
   const [allMemes, setAllMemes] = useState([]);
 
-  React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes))
+  useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes)
+    }
+    getMemes();
   })
 
   function getMemeImage() {
