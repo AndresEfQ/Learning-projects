9f83510d447c058ea6bbc208a5373c0451add183
AndresEfQ <andresf2641@gmail.com>
2022-06-27 18:17:10 -0500

    Update hold dice function

--- React/tenzies/src/App.js
+++ React/tenzies/src/App.js
@@ -6,36 +6,40 @@ import {nanoid} from "nanoid"
 export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice());
+
+  function generateNewDie() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    }
+  }
   
   function allNewDice() {
     let newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        id: nanoid(),
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false
-      });
+      newDice.push(generateNewDie());
     }
     
     return newDice;
   }
 
   function refreshDice() {
-    setDice(allNewDice());
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }));
   }
 
   function holdDice(id) {
     setDice(prevDice => prevDice.map(die => {
-      if (die.id === id) {
-        return {...die, isHeld: !die.isHeld};
-      }
-
-      return die;
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die;
     }));
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {dice.map(die => <Die 
           key={die.id}
