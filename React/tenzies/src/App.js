609d2b3afc3e5f1640cb439a2ee989251b353e0e
AndresEfQ <andresf2641@gmail.com>
2022-06-27 15:01:41 -0500

    Create state and roll dice function

--- React/tenzies/src/App.js
+++ React/tenzies/src/App.js
@@ -2,23 +2,29 @@ import React from "react";
 import "./styles.css";
 import Die from "./components/Die"
 
-function App() {
+export default function App() {
+
+  const [dice, setDice] = React.useState(allNewDice());
+  
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    
+    return newDice;
+  }
+
+  function refreshDice() {
+    setDice(allNewDice());
+  }
+
   return (
     <main>
       <div className="dice-container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {dice.map(die => <Die value={die} />)}
       </div>
+      <button className="roll-dice" onClick={refreshDice}>Roll</button>
     </main>
   );
 }
-
-export default App;
