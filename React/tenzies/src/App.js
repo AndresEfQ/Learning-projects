82ee0fbb7b6c5290bcc3922e0980d2e49061b29e
AndresEfQ <andresf2641@gmail.com>
2022-06-28 18:47:39 -0500

    Implement score and best score in local storage

--- React/tenzies/src/App.js
+++ React/tenzies/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect, useRef } from "react";
 import "./styles.css";
 import Die from "./components/Die";
 import {nanoid} from "nanoid";
@@ -7,19 +7,31 @@ import Confetti from "react-confetti";
 export default function App() {
 
   const valueArray = ["one", "two", "three", "four", "five", "six"]
-  const [dice, setDice] = React.useState(allNewDice());
-  const [tenzies, setTenzies] = React.useState(false);
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  const [score, setScore] = useState({rolls: 0, time: 0})
 
-  React.useEffect (() => {
+  useEffect (() => {
     const allHeld = dice.every(die => die.isHeld);
     const allSameValue = dice.every(die => die.value === dice[0].value)
 
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log("You won!");
     }
-    console.log(dice);
+    console.log(score);
   }, [dice])
+
+  useEffect(() => {
+    let bestScore = JSON.parse(localStorage.getItem('bestScore')) || {rolls: Infinity, time: 0};
+
+    if (tenzies) {
+      if (score.rolls < bestScore.rolls) {
+        bestScore = score
+        localStorage.setItem('bestScore', JSON.stringify(score))
+      }
+    }
+    console.log("best score " + bestScore.rolls);
+  }, [tenzies])
 
   function generateNewDie() {
     return {
@@ -42,7 +54,10 @@ export default function App() {
     if (tenzies) {
       setDice(allNewDice());
       setTenzies(false);
+      setScore({rolls: 0, time: 0})
+
     } else {
+      setScore(prevScore => ({...prevScore, rolls: prevScore.rolls + 1}))
       setDice(prevDice => prevDice.map(die => {
         return die.isHeld ? die : generateNewDie();
       }));
@@ -71,6 +86,24 @@ export default function App() {
       <button className="roll-dice" onClick={refreshDice}>
         {tenzies ? "New Game" : "Roll"}
       </button>
+      <section>
+        <div>
+          <div>
+            Your Score: {score.rolls}
+          </div>
+          <div>
+            Best Score: {JSON.parse(localStorage.getItem('bestScore')).rolls}
+          </div>
+        </div>
+        <div>
+          <div>
+            Your Time: {score.rolls}
+          </div>
+          <div>
+            Best Time: {JSON.parse(localStorage.getItem('bestScore')).rolls}
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
