947982b064ef7e95927b17ca9d0b26df47a7d65d
AndresEfQ <andresf2641@gmail.com>
2022-07-17 22:25:10 -0500

    Fix focus, working on validation

--- HTML-CSS/odin-sign-up/script.js
+++ HTML-CSS/odin-sign-up/script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const inputs = Array.from(document.getElementsByTagName('input'));
+console.log(inputs);
+
+for (input of inputs) {
+  input.addEventListener('change', (e) => fill(e));
+}
+
+function fill(e) {
+
+  e.target.classList.add('filled');
+  console.log(element.name + ' filled');
+}
