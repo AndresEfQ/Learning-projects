6932b1f497619bb0a5eba6c230a189d3d3eff32b
AndresEfQ <andresf2641@gmail.com>
2022-07-18 22:34:29 -0500

    Finish validation and format in chrome

--- HTML-CSS/odin-sign-up/script.js
+++ HTML-CSS/odin-sign-up/script.js
@@ -1,12 +1,74 @@
 const inputs = Array.from(document.getElementsByTagName('input'));
 console.log(inputs);
 
+const errors = Array.from(document.querySelectorAll('div span:first-child'));
+
+const form = document.querySelector('form');
+const pwd = document.getElementById('pwd');
+const confirmPwd = document.getElementById('confirm');
+const button = document.querySelector('button');
+
+form.addEventListener('submit', (e) => {
+  if (!inputs.every(element => element.validity.valid)) {
+    e.preventDefault();
+  }
+  console.log('submited');
+})
+
+button.addEventListener('click', () => {
+  for (input of inputs) {
+    if (input.validity.valueMissing) {
+      input.classList.add('filled');
+      input.nextElementSibling.classList.add('visible');
+    }
+  }
+});
+
 for (input of inputs) {
-  input.addEventListener('change', (e) => fill(e));
+  input.addEventListener('change', (e) => {
+    e.target.classList.add('filled');
+
+    if (e.target.validity.valueMissing) {
+      e.target.nextElementSibling.classList.add('visible');
+    }
+  });
 }
 
-function fill(e) {
+for (input of inputs) {
+  input.addEventListener('change', (e) => {
 
-  e.target.classList.add('filled');
-  console.log(element.name + ' filled');
+    if (e.target.validity.patternMismatch || e.target.validity.typeMismatch) {
+      e.target.nextElementSibling.nextElementSibling.classList.add('visible');
+    }
+  });
 }
+
+for (input of inputs) {
+  input.addEventListener('input', (e) => {
+    if (!e.target.validity.valueMissing) {
+      e.target.nextElementSibling.classList.remove('visible');
+    }
+  });
+}
+
+confirmPwd.addEventListener('input', () => {
+  if (confirmPwd.value) {
+    if (pwd.value !== confirmPwd.value) {
+      pwd.nextElementSibling.nextElementSibling.classList.add('visible');
+    } 
+    if (pwd.value === confirmPwd.value) {
+      pwd.nextElementSibling.nextElementSibling.classList.remove('visible');
+    }
+  }
+});
+
+pwd.addEventListener('change', () => {
+  if (confirmPwd.value) {
+    if (pwd.value !== confirmPwd.value) {
+      pwd.nextElementSibling.nextElementSibling.classList.add('visible');
+    } 
+    if (pwd.value === confirmPwd.value) {
+      pwd.nextElementSibling.nextElementSibling.classList.remove('visible');
+    }
+  }
+});
