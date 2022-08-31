360c66614b31dd97173b50bdd873f294b5db1aed
AndresEfQ <andresf2641@gmail.com>
2022-08-30 21:12:26 -0500

    Complete multiplayer and easy single mode

--- VanillaJS/ticTacToe/script.js
+++ VanillaJS/ticTacToe/script.js
@@ -149,81 +149,173 @@ Array.from(
 
 const gameBoard = (() => {
   let board = new Array(9).fill(null);
-  const checkWinner = () => {
+  let emptyBoxes;
+  const winnerMessage = document.getElementById('winner-message');
+  const winnerDiv = document.getElementById('winner');
+  const displayBoxes = Array.from(document.getElementsByClassName('box'));
 
+  const checkWinner = (board, player) => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    return lines.some(line => line.every(box => board[box] == player.mark))
   }
-  const checkTie = () => {
-
+  const checkTie = (board) => {
+    const emptyBoxes = board.filter(box => box == null);
+    if (emptyBoxes.length == 0) return true;
   }
-  const setMove = () => {
-
+  const showResult = (board, player) => {
+    let message;
+    if (checkWinner(board, player)) {
+      message = `${player.name} WINS!!`; 
+    } else if (checkTie(board)) {
+      message = `It's a tie`;
+    } else {
+      return;
+    }
+    winnerMessage.textContent = message;
+    winnerDiv.style.display = 'flex';
+  } 
+  const move = (player, index) => {
+    if (board[index]) return;
+    board[index] = player.mark;
+    emptyBoxes = board.reduce((prev, curr, currIndex) => {
+      if (curr == null) {
+        prev.push(currIndex);
+      }
+      return prev;
+    }, []);
+    displayBoxes[index].textContent = `${player.mark}`;
+    displayBoxes[index].classList = `box ${player.markColor}`;
+    showResult(board, player);
+    displayController.toogleActivePlayer();
+    return true;
+  }
+  const getEmptyBoxes = () => {
+    return emptyBoxes;
   }
   return {
-
+    board,
+    checkWinner,
+    checkTie,
+    move,
+    getEmptyBoxes
   }
 })();
 
 const displayController = (() => {
-  let mode;
-  let difficulty;
   let player1;
   let player2;
+  let activePlayer;
+  let inactivePlayer;
 
-  const startGame = () => {
-    mode = document.getElementById('game-mode').value;
-    difficulty = document.getElementById('difficulty').value;
+  const assignStartingTurn = (player) => {
+    activePlayer = player;
+    inactivePlayer = player == player1 ? player2 : player1;
+    showActivePlayer(activePlayer);
+    player.play();
+  }
+  const toogleActivePlayer = () => {
+    inactivePlayer = activePlayer;
+    activePlayer = activePlayer == player1 ? player2 : player1;
+    showActivePlayer(activePlayer);
+    activePlayer.play();
+  }
+  const showActivePlayer = (player) => {
+    const activePlayerName = document.getElementById(`${player.id}`);
+    const inactivePlayerName = document.getElementById(`${inactivePlayer.id}`);
+    activePlayerName.className = 'animate-character';
+    inactivePlayerName.className = '';
+  }
+  /* const setActivePlayer = (player) => {
+    activePlayer = player
+    showActivePlayer();
+  } */
+  const startGame = (mode, difficulty) => {
     document.querySelector(".configuration").style.display = "none";
-    player1 = player('player1', 'player1', 'human', 'X');
+    player1 = player('player1', 'Player 1', 'human', 'X');
     let player2Type = mode == 'single' ? 'ia' : 'human';
-    console.log(getDifficulty());
-    player2 = player('player2', 'player2', player2Type, 'O', difficulty);
+    player2 = player('player2', 'Player 2', player2Type, 'O', difficulty);
+    assignStartingTurn(player1); // Here I can change who starts
   }
-  const assignTurn = () => {
-
-  }
-  const getPlayer2 = () => {
-    return player2;
-  }
-  const getPlayer1 = () => {
+  const getPlayer1 = function() { //only to check if working
     return player1;
   }
-  const getDifficulty = () => {
-    return difficulty
+
+  const getActivePlayer = () => {
+    return activePlayer;
   }
+
   return {
     startGame,
-    getPlayer2,
-    getPlayer1,
-    getDifficulty
+    getPlayer1, //may delete
+    /* setActivePlayer, */
+    getActivePlayer,
+    toogleActivePlayer,
+    assignStartingTurn
   }
 })();
 
 const human = () => {
-  const play = () => {
-    console.log('human playing')
+  let index;
+  const displayBoard = document.querySelector('.gameboard');
+  let bindedHandler;
+
+  const play = function() {
+    bindedHandler = handleHumanMove.bind(this);
+    displayBoard.addEventListener('click', bindedHandler);
   }
-  return {play};
+  const handleHumanMove = function(e) {
+    index = e.target.dataset['index'];
+    let moved = gameBoard.move(this, index);
+    if (moved) displayBoard.removeEventListener('click', bindedHandler);
+  }
+  return { play };
 }
 
 const ia = (difficulty) => {
-  let play
+  let play;
+  let index;
 
+  const easyPlay = function() {
+    let emptyBoxes = gameBoard.getEmptyBoxes();
+    if (emptyBoxes.length == 0) return;
+    console.log(emptyBoxes);
+    let random = Math.floor(Math.random()*emptyBoxes.length)
+    index = emptyBoxes[random];
+    gameBoard.move(this, index);
+    /* do {
+      index = Math.floor(Math.random()*9);
+      console.log(index);
+      console.log(emptyBoxes);
+    } while (!(emptyBoxes.some(box => box == index)))
+    console.log(index)
+    gameBoard.move(this, index); */
+  }
   switch (difficulty) {
     case 'easy':
-      play = () => {console.log('ia is playing easy')};
+      play = easyPlay;
       break;
     case 'extreme':
       play = () => {console.log('ia is playing extreme')};
       break;
   }
-  return {play};
-} //not working as espected
+
+  return { play };
+}
 
 const player = (id, name, type, mark, difficulty) => {
   const {play} = type == 'ia' ? ia(difficulty) : human();
+  const markColor = mark == 'X' ? 'black' : 'red';
+  /* const getMark = () => {
+    return mark;
+  }
+  const getMarkColor = () => {
+    return markColor;
+  } */
 
   return {
     play,
+    mark,
+    markColor,
     id,
     name,
     type,
@@ -232,4 +324,5 @@ const player = (id, name, type, mark, difficulty) => {
   }
 };
 
-document.querySelector('.play').addEventListener('click', displayController.startGame);
+const options = Array.from(document.getElementsByClassName('game-option'));
+options.forEach(option => option.addEventListener('click', () => displayController.startGame(option.dataset['mode'], option.dataset['difficulty'])));
