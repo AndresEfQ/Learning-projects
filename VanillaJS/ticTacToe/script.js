b0017810f4c796c37f0db42527528a057b982cdb
AndresEfQ <andresf2641@gmail.com>
2022-09-03 18:48:29 -0500

    Complete extreme play, todo hard and medium

--- VanillaJS/ticTacToe/script.js
+++ VanillaJS/ticTacToe/script.js
@@ -1,158 +1,9 @@
-/* const gameBoard = (() => {
-  const winnerMessage = document.getElementById('winner-message');
-  const winnerDiv = document.getElementById('winner');
-  const screen = Array.from(document.getElementsByClassName('box'));
-  let board = new Array(9).fill(null);
-  let inactivePlayer;
-
-  const showMove = (index, mark, markColor) => {
-    if (board[index]) return;
-    board[index] = mark;
-    screen[index].textContent = mark;
-    screen[index].classList = `box ${markColor}`
-  }
-  const showResults = (winner) => {
-    const message = winner ? `Congratulations ${winner.getName()}`: `It's a draw`; 
-    winnerMessage.textContent = message;
-    winnerDiv.style.display = 'flex';
-  }
-  const hideResults = () => {
-    winnerDiv.style.display = 'none';
-  }
-  const showActivePlayer = (activePlayer) => {
-    console.log(activePlayer == player1);
-    inactivePlayer = activePlayer == player1 ? player2 : player1;
-    const activePlayerName = document.getElementById(`${activePlayer.getId()}`);
-    const inactivePlayerName = document.getElementById(`${inactivePlayer.getId()}`);
-    activePlayerName.classList.add('animate-character');
-    inactivePlayerName.classList.remove('animate-character');
-  }
-  const cleanBoard = () => {
-    board.fill(null);
-    screen.forEach((box) => {
-      box.textContent = '';
-      box.classList = 'box'
-    })
-    hideResults();
-  }
-
-  return {
-    board,
-    showMove,
-    showResults,
-    showActivePlayer,
-    cleanBoard,
-    inactivePlayer
-  };
-})();
-
-const controler = (() => {
-  const gameBoardElement = document.querySelector(".gameboard");
-  let mode;
-  let activePlayer;
-  const hideConfig = () => {
-    document.querySelector(".configuration").style.display = "none";
-  }
-  const setMode = () => {
-    mode = document.getElementById('game-mode').value;
-    console.log(mode);
-  }
-  const getMode = () => {
-    return mode;
-  }
-  const startGame = () => {
-    hideConfig();
-    gameBoard.cleanBoard();
-    gameBoardElement.addEventListener("click", controler.makeMove);
-  }
-  const setActivePlayer = (player) => {
-    activePlayer = player
-    gameBoard.showActivePlayer(activePlayer);
-  }
-  const toogleActivePlayer = () => {
-    activePlayer = activePlayer == player1 ? player2 : player1;
-    gameBoard.showActivePlayer(activePlayer);
-  }
-  const checkWinner = () => {
-    let winner;
-    const board = gameBoard.board;
-    const lines = [[0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]]
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (board[a] && board[a] == board[b] && board[a] == board[c]) {
-        winner = board[a] == player1.mark ? player1 : player2;
-      }
-    }
-    if (winner) {
-      gameBoard.showResults(winner);
-      gameBoardElement.removeEventListener('click', controler.makeMove);
-    }
-    if (board.every(Boolean)) gameBoard.showResults();
-  }
-  const makeMove = (e) => {
-    activePlayer.play(e);
-    checkWinner();
-    toogleActivePlayer();
-  }
-
-  return {
-    startGame,
-    getMode,
-    setMode,
-    toogleActivePlayer,
-    setActivePlayer,
-    makeMove,
-  };
-})();
-
-const player = (id, type, name, mark) => {
-  const nameElement = document.getElementById(`${name}`);
-  const markColor = mark == 'O' ? 'red-color' : 'black-color';
-  const setName = (newName) => {
-    name = newName;
-  }
-  const getName = () => {
-    return name;
-  }
-  const getId = () => {
-    return id;
-  }
-  const play = (e) => {
-    const index = e.target.dataset['index'];
-    gameBoard.showMove(index, mark, markColor);
-  }
-  nameElement.addEventListener('change', (e) => {
-    setName(e.target.value);
-  })
-
-  return {
-    getName,
-    getId,
-    play,
-    mark
-  }
-}
-
-let player1 = player('player1', 'human', 'player1', 'X');
-let player2 = player('player2', 'human', 'player2', 'O');
-controler.setActivePlayer(player1);
-
-Array.from(
-  document.getElementsByClassName("play")).
-  forEach((el) => el.addEventListener("click", () => {
-    controler.startGame();
-    controler.setMode();
-  })
-); */
-
 const gameBoard = (() => {
   let board = new Array(9).fill(null);
-  //let emptyBoxes;
   const winnerMessage = document.getElementById('winner-message');
   const winnerDiv = document.getElementById('winner');
   const displayBoxes = Array.from(document.getElementsByClassName('box'));
+  let gameFinished;
 
   const checkWinner = (board, player) => {
     const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
@@ -176,31 +27,20 @@ const gameBoard = (() => {
     return true;
   } 
   const move = (player, index) => {
-    if (board[index]) return;
+    if (gameFinished || board[index]) return;
     board[index] = player.mark;
-    /*emptyBoxes = board.reduce((prev, curr, currIndex) => {
-      if (curr == null) {
-        prev.push(currIndex);
-      }
-      return prev;
-    }, []);*/
     displayBoxes[index].textContent = `${player.mark}`;
     displayBoxes[index].classList = `box ${player.markColor}`;
-    let gameFinished = showResult(board, player);
+    gameFinished = showResult(board, player);
     if (!gameFinished) {
       displayController.toogleActivePlayer();
     }
     return true;
   }
-  /*const getEmptyBoxes = () => {
-    return emptyBoxes;
-  }*/
   return {
     board,
-    // checkWinner,
-    // checkTie,
     move,
-    //getEmptyBoxes
+    checkWinner
   }
 })();
 
@@ -228,10 +68,6 @@ const displayController = (() => {
     activePlayerName.className = 'animate-character';
     inactivePlayerName.className = '';
   }
-  /* const setActivePlayer = (player) => {
-    activePlayer = player
-    showActivePlayer();
-  } */
   const startGame = (mode, difficulty) => {
     document.querySelector(".configuration").style.display = "none";
     player1 = player('player1', 'Player 1', 'human', 'X');
@@ -239,21 +75,23 @@ const displayController = (() => {
     player2 = player('player2', 'Player 2', player2Type, 'O', difficulty);
     assignStartingTurn(player1); // Here I can change who starts
   }
-  const getPlayer1 = function() { //only to check if working
-    return player1;
-  }
 
-  const getActivePlayer = () => {
-    return activePlayer;
-  }
+  // delete
+  const getPlayer = (n) => {
+    if (n == 1) {
+      return player1; 
+    } else if (n == 2) {
+      return player2;
+    } else {
+      return;
+    }
+  } 
 
   return {
     startGame,
-    getPlayer1, //may delete
-    /* setActivePlayer, */
-    getActivePlayer,
     toogleActivePlayer,
-    assignStartingTurn
+    assignStartingTurn,
+    getPlayer
   }
 })();
 
@@ -278,7 +116,100 @@ const ia = (difficulty) => {
   let play;
   let index;
 
+  const minimax = function(board, player, isMax, depth) {
+    let score;
+    if (gameBoard.checkWinner(board, {mark: 'O'})) {
+      score = 10 - depth;
+      return score;
+    } else if (gameBoard.checkWinner(board, {mark: 'X'})) {
+      score = -10 + depth;
+      return score;
+    } else if (board.filter(box => box == null).length == 0) {
+      score = 0;
+      return score;
+    }
+  
+    let moves = [];
+    let partialScore;
+    let nextPlayer = {};
+    nextPlayer.mark = player.mark == 'X' ? 'O' : 'X';
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == null) {
+        board[i] = nextPlayer.mark;
+        partialScore = minimax(board, nextPlayer, !isMax, depth + 1);
+        board[i] = null;
+        moves.push(partialScore);
+      }
+    }
+  
+    if (isMax) {
+      let max = -1000;
+      for (let i = 0; i < moves.length; i++) {
+        max = Math.max(max, moves[i]);
+      }
+      score = max;
+    } else {
+      let min = 1000;
+      for (let i = 0; i < moves.length; i++) {
+        min = Math.min(min, moves[i]);
+      }
+      score = min;
+    }
+    return score;
+  }
+
+  const bestMove = function(board, player) {
+    let moves = [];
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == null) {
+        let move = {};
+        move.index = i;
+        board[i] = player.mark;
+        move.score = minimax(board, player, false, 0);
+        board[i] = null;
+        moves.push(move);
+      }
+    }
+    let max = -1000;
+    let index;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > max) {
+        max = moves[i].score;
+        index = moves[i].index;
+      }
+    }
+    return index;
+  }
+
+  const extremePlay = function() {
+    console.log('extreme');
+    const board = gameBoard.board;
+    const index = bestMove(board, this);
+    gameBoard.move(this, index);
+  }
+  
+  const midPlay = function() {
+    let randomPlay = Math.floor(Math.random()*3);
+    console.log(randomPlay);
+    if (randomPlay < 2) {
+      easyPlay();
+    } else {
+      extremePlay();
+    }
+  }
+
+  const hardPlay = function() {
+    let randomPlay = Math.floor(Math.random()*3);
+    console.log(randomPlay);
+    if (randomPlay < 1) {
+      return easyPlay;
+    } else {
+      return extremePlay;
+    }
+  }
+
   const easyPlay = function() {
+    console.log('easy')
     let emptyBoxes = gameBoard.board.reduce((prev, curr, currIndex) => {
       if (curr == null) {
         prev.push(currIndex);
@@ -286,24 +217,23 @@ const ia = (difficulty) => {
       return prev;
     }, []);
     if (emptyBoxes.length == 0) return;
-    console.log({emptyBoxes});
     let random = Math.floor(Math.random()*emptyBoxes.length)
     index = emptyBoxes[random];
     gameBoard.move(this, index);
-    /* do {
-      index = Math.floor(Math.random()*9);
-      console.log(index);
-      console.log(emptyBoxes);
-    } while (!(emptyBoxes.some(box => box == index)))
-    console.log(index)
-    gameBoard.move(this, index); */
   }
+
   switch (difficulty) {
+    case 'extreme':
+      play = extremePlay;
+      break;
+    case 'hard':
+      play = hardPlay;
+      break;
+    case 'medium':
+      play = midPlay;
+      break;
     case 'easy':
       play = easyPlay;
-      break;
-    case 'extreme':
-      play = () => {console.log('ia is playing extreme')};
       break;
   }
 
@@ -313,22 +243,13 @@ const ia = (difficulty) => {
 const player = (id, name, type, mark, difficulty) => {
   const {play} = type == 'ia' ? ia(difficulty) : human();
   const markColor = mark == 'X' ? 'black' : 'red';
-  /* const getMark = () => {
-    return mark;
-  }
-  const getMarkColor = () => {
-    return markColor;
-  } */
 
   return {
     play,
     mark,
     markColor,
     id,
-    name,
-    type,
-    mark,
-    difficulty
+    name
   }
 };
 
